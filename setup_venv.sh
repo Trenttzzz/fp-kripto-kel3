4d3aa294123f6b5c9b6c5c3d9e579d258f48b7d0
Munsven <150172271+Munvens@users.noreply.github.com>
2025-06-16 14:50:11 +0700

    code: add setup_venv for python environment

---
+++ setup_venv.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Nama folder venv
+VENV_DIR=".fpkripto"
+
+# Cek apakah folder venv sudah ada
+if [ ! -d "$VENV_DIR" ]; then
+    echo "Virtual environment belum ada. Membuat venv..."
+    python3 -m venv $VENV_DIR
+
+    if [ $? -ne 0 ]; then
+        echo "Gagal membuat virtual environment!"
+        exit 1
+    fi
+else
+    echo "Virtual environment sudah ada."
+fi
+
+# Aktifkan virtual environment
+echo "Mengaktifkan virtual environment..."
+
+source $VENV_DIR/bin/activate
